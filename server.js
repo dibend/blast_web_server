bea1a5459d9a9714667d6598cc84998859f75a7d
David DiBenedetto <dibend8@gmail.com>
2017-05-15 15:12:00 +0000

    survey style

--- server.js
+++ server.js
@@ -782,6 +782,54 @@ app.get('/confirm_eb_manhattan_parties', function(request, response) {
   }
 });
 
+var executive_orders_confirmEmailQuery = {};
+app.get('/signup_executive_orders', function(request, response) {
+  var email = request.query.email;
+  if (emailValidator.validate(email)) {
+    var secret = crypto.randomBytes(64).toString('hex');
+    executive_orders_confirmEmailQuery[secret] = email;
+
+    var mailOptions = {
+      from: 'Blast Notifications <blasts@blastnotifications.com>',
+      to: email,
+      subject: 'Confirm White House Executive Orders Blast Notification',
+      text: 'Visit https://blastnotifications.com/confirm_executive_orders?secret=' + secret + ' to verify your subscription!'
+    };
+
+    mailer.sendMail(mailOptions, function(err, res) {
+      if(err) {
+        console.log(err);
+      }
+      mailer.close();
+    });
+    console.log(email + ' confirmation sent');
+    response.redirect('/confirm.html');
+  } else {
+    response.status(404);
+    response.sendFile(path.join(__dirname+'/public/404.html'));
+  }
+});
+
+app.get('/confirm_executive_orders', function(request, response) {
+  var secret = request.query.secret;
+  if(secret in executive_orders_confirmEmailQuery) {
+    var email = executive_orders_confirmEmailQuery[secret];
+
+    db.query('INSERT IGNORE INTO executive_orders SET ?', {email: email}, function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+
+    response.redirect('/confirmed.html');
+    console.log(email + ' confirmed');
+    delete executive_orders_confirmEmailQuery[secret];
+  } else {
+    response.status(404);
+    response.sendFile(path.join(__dirname+'/public/404.html'));
+  }
+});
+
 app.get('*', function(request, response) {
   response.status(404);
   response.sendFile(path.join(__dirname+'/public/404.html'));
