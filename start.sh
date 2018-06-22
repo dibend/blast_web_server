3546a1ee441cce606d0c99f2f947bce0d4b06fe2
David DiBenedetto <dibend8@gmail.com>
2018-06-22 23:58:34 +0000

    fix comment, gitignore feeds

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Redirect port 80 (http) to 8080 (http server)
-# Redirect port 443 (http) to 8443 (https server)
+# Redirect port 443 (https) to 8443 (https server)
 # Allows express server to be run as regular user for security measures
 
 sudo iptables -t nat -D PREROUTING 1
