4845b203f87b1bac3460536c14aed90b2bbf9a05
David DiBenedetto <dibend8@gmail.com>
2018-10-19 20:00:08 +0000

    cnet

--- server.js
+++ server.js
@@ -1416,6 +1416,54 @@ app.get('/confirm_reuters_tech', function(request, response) {
   }
 });
 
+var cnet_confirmEmailQuery = {};
+app.get('/signup_cnet', function(request, response) {
+  var email = request.query.email;
+  if (emailValidator.validate(email)) {
+    var secret = crypto.randomBytes(64).toString('hex');
+    cnet_confirmEmailQuery[secret] = email;
+
+    var mailOptions = {
+      from: config.from,
+      to: email,
+      subject: 'Confirm CNET News Blast Notification',
+      text: 'Visit https://blastnotifications.com/confirm_cnet?secret=' + secret + ' to verify your subscription!'
+    };
+
+    mailer.sendMail(mailOptions, function(err, res) {
+      if(err) {
+        console.log(err);
+      }
+      mailer.close();
+    });
+    console.log(email + ' confirmation sent');
+    response.redirect('/confirm.html');
+  } else {
+    response.status(404);
+    response.sendFile(path.join(__dirname+'/public/404.html'));
+  }
+});
+
+app.get('/confirm_cnet', function(request, response) {
+  var secret = request.query.secret;
+  if(secret in cnet_confirmEmailQuery) {
+    var email = cnet_confirmEmailQuery[secret];
+
+    db.query('INSERT IGNORE INTO cnet SET ?', {email: email}, function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+
+    response.redirect('/confirmed.html');
+    console.log(email + ' confirmed');
+    delete cnet_confirmEmailQuery[secret];
+  } else {
+    response.status(404);
+    response.sendFile(path.join(__dirname+'/public/404.html'));
+  }
+});
+
 app.get('*', function(request, response) {
   response.redirect('/');
 });
