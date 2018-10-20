24514ecf953410799821a01a7613f08f437e8893
David DiBenedetto <dibend8@gmail.com>
2018-10-20 21:55:49 +0000

    currency direct

--- server.js
+++ server.js
@@ -1514,6 +1514,54 @@ app.get('/confirm_marketwatch_currency', function(request, response) {
   }
 });
 
+var currency_direct_confirmEmailQuery = {};
+app.get('/signup_currency_direct', function(request, response) {
+  var email = request.query.email;
+  if (emailValidator.validate(email)) {
+    var secret = crypto.randomBytes(64).toString('hex');
+    currency_direct_confirmEmailQuery[secret] = email;
+
+    var mailOptions = {
+      from: config.from,
+      to: email,
+      subject: 'Confirm Currency Direct News Blast Notification',
+      text: 'Visit https://blastnotifications.com/confirm_currency_direct?secret=' + secret + ' to verify your subscription!'
+    };
+
+    mailer.sendMail(mailOptions, function(err, res) {
+      if(err) {
+        console.log(err);
+      }
+      mailer.close();
+    });
+    console.log(email + ' confirmation sent');
+    response.redirect('/confirm.html');
+  } else {
+    response.status(404);
+    response.sendFile(path.join(__dirname+'/public/404.html'));
+  }
+});
+
+app.get('/confirm_currency_direct', function(request, response) {
+  var secret = request.query.secret;
+  if(secret in currency_direct_confirmEmailQuery) {
+    var email = currency_direct_confirmEmailQuery[secret];
+
+    db.query('INSERT IGNORE INTO currency_direct SET ?', {email: email}, function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+
+    response.redirect('/confirmed.html');
+    console.log(email + ' confirmed');
+    delete currency_direct_confirmEmailQuery[secret];
+  } else {
+    response.status(404);
+    response.sendFile(path.join(__dirname+'/public/404.html'));
+  }
+});
+
 app.get('*', function(request, response) {
   response.redirect('/');
 });
